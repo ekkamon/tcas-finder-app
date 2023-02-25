ebce87b0a0001ef1dbd89fb4766e6a20da127cb4
Z4C0N <34152283+ekkamon@users.noreply.github.com>
2023-02-25 12:24:47 +0700

    [fix/remove] find single to many | clear files.

--- utils/DataManager.ts
+++ utils/DataManager.ts
@@ -4,11 +4,11 @@ export const findUniversity = (university_id: string | null) => {
   return UniversityData.find((v) => v.university_id == university_id);
 };
 
-export const findSingleFaculty = (
+export const findManyFaculty = (
   university_id: string | null,
   faculty_id: string | null
 ) => {
-  return findUniversity(university_id)?.facultys.find(
+  return findUniversity(university_id)?.facultys.filter(
     (v) => v.faculty_id == faculty_id
   );
 };
