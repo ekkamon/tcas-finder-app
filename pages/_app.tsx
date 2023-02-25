e379c9ccc494f7dfa824808dc2553a9a1e16e1e4
Z4C0N <34152283+ekkamon@users.noreply.github.com>
2023-02-25 23:15:38 +0700

    [add] header & footer.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,21 @@
 import '../styles/globals.css';
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
 import { Navbar } from '../components/Navbar';
+import { Footer } from '../components/Footer';
+import { GlobalProvider } from '../contexts/GlobalContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Navbar />
-      <Component {...pageProps} />
+      <Head>
+        <title>TCASFINDER</title>
+      </Head>
+      <GlobalProvider>
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+      </GlobalProvider>
     </>
   );
 }
