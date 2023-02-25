105cd0192f507b5562231c1379fadad4dc3d56c0
Z4C0N <34152283+ekkamon@users.noreply.github.com>
2023-02-25 23:57:49 +0700

    [fix] footer responsive.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,8 +2,8 @@ import { FC } from 'react';
 
 export const Footer: FC = () => {
   return (
-    <div className="fixed bottom-0 w-full">
-      <div className="py-5 text-center text-xs">
+    <div className="w-full">
+      <div className="mt-[17%] mb-[15%] sm:mb-0 text-center text-xs">
         เว็บไซต์นี้สร้างขึ้นมาเพื่อเป็นการศึกษาเท่านั้นไม่มีเจตนาอื่นๆ
       </div>
     </div>
